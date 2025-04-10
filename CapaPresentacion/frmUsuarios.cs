6e76ec2eb5f77a7e70b35fe88d654ea88f9d910f
ezzz3 <ezzz3@DESKTOP-IJ001PS>
2025-04-10 09:50:50 -0300

    Revert "Mejoras en la validación y UI de proveedores"
    This reverts commit 97668c88b8fa6407091905984ac039fe5ce7e1bc.

--- CapaPresentacion/frmUsuarios.cs
+++ CapaPresentacion/frmUsuarios.cs
@@ -250,7 +250,7 @@ namespace CapaPresentacion
 
         private void btneliminar_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(txtid.Text) && int.TryParse(txtid.Text, out int id) && id != 0)
+            if (Convert.ToInt32(txtid.Text) != 0)
             {
                 if (MessageBox.Show("Desea eliminar el usuario?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
